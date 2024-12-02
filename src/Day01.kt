325e9f30c1e2948916840d2531f81da186138e3d
Marko Sopčić <marko.sopcic@gmail.com>
2024-12-02 12:38:08 +0100

    Add Day one.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,23 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Long {
+        val (first, second) = input.map { it.split("\\s+".toRegex()).withIndex() }.flatten().groupBy { it.index }
+            .map { it.value.map { it.value.toLong() }.sorted() }
+        return first.zip(second).sumOf { abs(it.first - it.second) }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val (first, second) = input.map { it.split("\\s+".toRegex()).withIndex() }.flatten().groupBy { it.index }
+            .map { it.value.map { it.value.toLong() } }
+
+        val secondMap = mutableMapOf<Long, Int>()
+        for(num in second){
+            secondMap[num] = secondMap.getOrDefault(num, 0) + 1
+        }
+        return first.sumOf { it * secondMap.getOrDefault(it, 0) }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
